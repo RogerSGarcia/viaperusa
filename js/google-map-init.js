d7dd226548ddbcfae0c67b5d1756098bc6035fe4
RogerSGarcia <rogervia@hotmail.com>
2020-04-30 17:41:21 -0500

    adding website

---
+++ js/google-map-init.js
@@ -0,0 +1,142 @@
+jQuery(document).ready(function() {
+    "use strict";
+
+
+    function b() {
+        var a = {
+                zoom: 10,
+                scrollwheel: false,
+                center: new google.maps.LatLng(40.662112, -74.211978),
+                styles: [{
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 65
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 51
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 30
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 40
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.province",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "lightness": -25
+                        },
+                        {
+                            "saturation": -100
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "hue": "#ffff00"
+                        },
+                        {
+                            "lightness": -25
+                        },
+                        {
+                            "saturation": -97
+                        }
+                    ]
+                }]
+            },
+            b = document.getElementById("map"),
+            c = new google.maps.Map(b, a);
+        new google.maps.Circle({
+              strokeColor: 'rgb(222,184,135)',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: 'rgb(222,184,135)',
+              fillOpacity: 0.35,
+              map: c,
+              center: {lat: 40.662112, lng: -74.211978},
+              radius: 16000
+        });
+    }
+    google.maps.event.addDomListener(window, "load", b);
+
+});
